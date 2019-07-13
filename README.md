c80082cbc9ae44c82af52b28403810d01cedc730
Ivan Avdonin <deshaion@gmail.com>
2019-07-14 00:30:38 +0300

    Some more examples were added to Readme

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ You can run it by command:
 ```
 ./gradlew bootRun
 ```
+
+How to make requests by curl
+```
+curl -i --user user:pass http://localhost:8080/employees/Jonas
+curl --user user:pass -d '{"Pete":"Nick", "Barbara":"Nick", "Nick":"Sophie", "Sophie":"Jonas"}' -H "Content-Type: application/json" -X POST http://localhost:8080/relationships
+```
--- src/main/java/com/personio/hierarchymanager/config/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/personio/hierarchymanager/config/CustomWebSecurityConfigurerAdapter.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -29,8 +30,9 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .httpBasic()
-                .authenticationEntryPoint(authenticationEntryPoint);
+                .httpBasic().authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
