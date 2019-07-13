1a8f6a9128eef455bb23f443aad91a0a91ddac56
Ivan Avdonin <deshaion@gmail.com>
2019-07-13 10:49:01 +0300

    First commit, empty spring boot application

---
+++ src/main/java/com/personio/hierarchymanager/HierarchyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personio.hierarchymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HierarchyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HierarchyManagerApplication.class, args);
+	}
+
+}
